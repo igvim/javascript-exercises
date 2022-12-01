f76cc7cbe55ac5d37d465e49791da8b5da6acaae
Isaiah <116988172+igvim@users.noreply.github.com>
2022-11-30 19:58:25 -0500

    added reverseString function, tests passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,21 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    /*
+    last char in first position
+    n -> 0
+    n - 1 -> 1
+    n - 2 -> 2
+    ...
+    0 -> n
+    Not sure if I can:
+    -subtract indices
+    -add chars to a string by calling the index
+    */
+   let rts = '';
+    let end = str.length-1;
+    for (let i = 0; i <= end; i++){
+        rts += str[end-i];
+    }
+    return rts;
 };
 
 // Do not edit below this line
