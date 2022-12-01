e5d22fff3db8373c78e80931fc43e8568dac1b39
Isaiah <116988172+igvim@users.noreply.github.com>
2022-11-30 21:46:33 -0500

    removeFromArray with nested for loops, solution uses forEach

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    /*
+    grab each element of the args array (args[i])
+    for each element of the entered array, if the values match,
+    remove the element (splice) at that position from the entered arr
+    if not a match do nothing
+    return the new array
+    */
+   for (let i = 0; i < args.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+        if (args[i] === arr[j]) arr.splice(j,1);
+    }
+   }
+   return arr;
 };
 
 // Do not edit below this line
