2abb97b9dccd44b653ada625c4bb928fea4f650b
Isaiah <116988172+igvim@users.noreply.github.com>
2022-11-30 19:05:36 -0500

    repeatString function passes all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num) {
+    let repeatStr = '';
+    if (num < 0) return 'ERROR';
+    for (i = 0; i < num; i++){
+        repeatStr = repeatStr.concat(string);
+    }
+    return repeatStr;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
