9c07aad3f7c4f411449327e1d58377e122d6631b
Isaiah <116988172+igvim@users.noreply.github.com>
2022-11-30 22:36:20 -0500

    sumAll function, constant time, without error handling

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+   const sumA = (a**2+a)/2;
+   const sumB = (b**2+b)/2;
+   let sum = 0;
+   if (a > b) {
+    let c = b-1
+    let smallerSum = (c**2+c)/2;
+    sum = sumA-smallerSum;
+    return sum;
+   }
+   else {
+    let c = a-1
+    let smallerSum = (c**2+c)/2;
+    sum = sumB-smallerSum;
+    return sum;
+   }
 };
 
 // Do not edit below this line
