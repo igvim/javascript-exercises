402450684c52a39a150f8ca10f689be8fe4e4308
Isaiah <116988172+igvim@users.noreply.github.com>
2022-12-01 21:21:12 -0500

    tempConversion, cake

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  c = (temp - 32) * 5/9;
+  return Math.round(c*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  f = 9/5 * temp + 32;
+  return Math.round(f*10)/10;
 };
 
 // Do not edit below this line
