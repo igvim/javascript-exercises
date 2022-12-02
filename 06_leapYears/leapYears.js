5e785828e780587b934d16fe961e08d44e5212e8
Isaiah <116988172+igvim@users.noreply.github.com>
2022-12-01 21:11:53 -0500

    leapYear passes all tests, multiple if statements

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(yr) {
+    if (yr % 100 == 0) {
+        if (yr % 400 == 0) {
+            return true;
+        }
+        else return false;
+    }
 
+    if (yr % 4 == 0) {
+        return true;
+    }
+    else return false;
 };
 
 // Do not edit below this line
