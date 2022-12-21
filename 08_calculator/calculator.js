13a6e22afebb4b419dc3a176e2505f9dc3f8f7b1
Isaiah <116988172+igvim@users.noreply.github.com>
2022-12-20 20:55:41 -0500

    all tests passing

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, item) => total + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, item) => product * item);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0) return 1;
+  let fact = 1;
+  for (let i = n; i > 0; i--) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
