3e2631eb8ec85bac4b5be2d9ab5fc739eb923c4d
Isaiah <116988172+igvim@users.noreply.github.com>
2022-12-20 23:03:32 -0500

    hard one

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n <= 0) return 'OOPS';
+    let a = 0;
+    let b = sum = 1;
+    for (i = 1; i < parseInt(n); i++) {
+        sum = a + b;
+        a = b;
+        b = sum;
+    }
+    return sum;
 };
 
 // Do not edit below this line
