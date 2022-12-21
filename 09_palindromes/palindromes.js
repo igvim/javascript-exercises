dfdf77ae5f5c06eb54e7b7340afe3f356c3c7001
Isaiah <116988172+igvim@users.noreply.github.com>
2022-12-20 21:56:24 -0500

    i like my solution better

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const cleanStr = str.toLowerCase().match(/[a-z]/g);
+    const reverse = [...cleanStr].reverse();
+    for (i = 0; i <= cleanStr.length; i++) {
+        if (cleanStr[i] !== reverse[i]) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
